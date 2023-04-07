80518cbed6dfc7a74480414a4e8af3ba5a30e2b9
Antonios Zachos <a.zachos@atc.gr>
2023-04-07 20:07:26 +0300

    upload frontend

---
+++ frontend/src/containers/UploadFiles.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+const UploadFiles = () => {
+    const [file, setFile] = useState(null)
+    const baseUrl = process.env.BASE_URL
+
+    const handleFileSelect = (event) => {
+        console.log('event', event)
+        setFile(event.target.files[0]);
+    }
+
+    const submitForm = (event) => {
+        event.preventDefault()
+
+        const formData = new FormData();
+        formData.append('image', file);
+        console.log('image', file)
+        console.log('formData', formData)
+
+        fetch(`http://localhost:3001/uploads`, {
+            method: 'POST',
+            body: formData,
+            // headers: {
+            //     'Content-Type': 'multipart/form-data'
+            //   }
+        }).then(response => {
+           return response.json()
+          }).then((data) => {
+            if(data.status !== 200) {
+                throw new Error(data.message)
+            }
+            alert(data.message)
+          })
+          .catch(error => {
+            console.error(error);
+            alert(error.message)
+          });
+    }
+  return (
+    <div>
+      <form method="POST" onSubmit={submitForm}>
+        <div>
+            <p>Upload file</p>
+            <input type='file' name='image' onChange={handleFileSelect}/>
+        </div>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default UploadFiles
